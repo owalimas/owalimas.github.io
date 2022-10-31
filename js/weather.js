0a19005965c46442e67bd68a8dc8812295d8fad8
owalimas <wj1204364750@qq.com>
2022-10-31 12:16:18 +0800

    Site updated: 2022-10-31 12:16:07

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "70348f5c159149bf886b41d954b607d2"
+  }
+}
